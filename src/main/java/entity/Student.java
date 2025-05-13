4d2203a7716b20938c385b4ea7a4473f03068ee2
BINARA <binaradilshanhettiarachchi@gmail.com>
2025-05-14 02:00:14 +0530

    Add entity class

---
+++ src/main/java/entity/Student.java
@@ -0,0 +1,52 @@
+package entity;
+
+public class Student {
+    private long id;
+    private String name;
+    private String address;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    private byte age;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+}
